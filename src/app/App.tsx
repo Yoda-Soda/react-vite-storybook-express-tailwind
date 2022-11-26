0491dc78987162dd5221c4a18227cf76072e1572
Jan Claasen <jan@cloudcannon.com>
2022-11-26 17:58:16 +1300

    Move header into its own component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,34 +2,40 @@ import React from 'react';
 import styles from './App.module.css';
 import { Link, BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './routes/home/Home';
+import Header from './components/Header/Header';
 
 function App(): JSX.Element {
   return (
-    <Router>
-      <div className={styles.App}>
-        <header>
-          <ul>
-            <Link to={'home'}>
-              <button>home</button>
-            </Link>
-            <Link to={'about'}>
-              <button>about</button>
-            </Link>
-          </ul>
-          <h1>header</h1>
-        </header>
-        <main className={styles['App-header']}>
-          <Switch>
-            <Route path="/about">
-              <main>About</main>
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </main>
-      </div>
-    </Router>
+    <>
+      <Header />
+      <body>
+        <Router>
+          <div className={styles.App}>
+            <header>
+              <ul>
+                <Link to={'home'}>
+                  <button>home</button>
+                </Link>
+                <Link to={'about'}>
+                  <button>about</button>
+                </Link>
+              </ul>
+              <h1>header</h1>
+            </header>
+            <main className={styles['App-header']}>
+              <Switch>
+                <Route path="/about">
+                  <main>About</main>
+                </Route>
+                <Route path="/">
+                  <Home />
+                </Route>
+              </Switch>
+            </main>
+          </div>
+        </Router>
+      </body>
+    </>
   );
 }
 
--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <head>
+      <meta charset="UTF-8" />
+      <link rel="icon" type="image/svg+xml" href="/src/app/favicon.svg" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Vite App</title>
+    </head>
+  );
 }
 
 export default Header;
